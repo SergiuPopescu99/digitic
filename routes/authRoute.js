04974212b8588dfd2f398caa08e3a8a820e19b04
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-06-16 22:36:54 +0300

    added auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, loginUserCtrl } = require('../controller/userCtrl')
-
+const { createUser, loginUserCtrl, getAllUsers, getSingleUser, deleteUser, updateUser, blockUser, unblockUser } = require('../controller/userCtrl');
+const { authMiddleware } = require("../middlewares/authMiddleware");
+const { isAdmin } = require('../middlewares/authMiddleware')
 router.post('/register', createUser);
 router.post('/login', loginUserCtrl)
+router.get('/all-users', getAllUsers);
+router.get('/:id', authMiddleware, isAdmin, getSingleUser);
+router.delete('/:id', deleteUser);
+router.put('/edit-user', authMiddleware, updateUser)
+router.put('/block-user/:id', authMiddleware, isAdmin, blockUser)
+router.put('/unblock-user/:id', authMiddleware, unblockUser)
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
