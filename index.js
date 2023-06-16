d7e93237bcffeb0284f94396e6cc46a14f074995
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-06-17 01:23:32 +0300

    added Product api

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ const app = express();
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute')
+const productRouter = require('./routes/productRoute')
 const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
-
+const cookieParser = require('cookie-parser')
 dbConnect();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use('/api/user', authRouter)
+app.use('/api/product', productRouter)
 app.use('/', (req, res) => {
     res.send('Hello from server side!')
 })
+
+
 app.use(notFound)
 app.use(errorHandler)
 
