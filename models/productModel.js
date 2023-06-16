d6ebb9d0f1032c18ff04352bdbfbb7493d714316
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-06-17 01:29:05 +0300

    added Product model

---
+++ models/productModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+
+let productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category"
+    },
+    brand: {
+        type: String,
+        enum: ['Apple', 'Samsung', 'Lenovo ']
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array
+    },
+    color: {
+        type: String,
+        enum: ['Black', 'Brown', 'Red']
+    }, ratings: [{
+        star: Number,
+        postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    }]
+
+}, { timestamps: true })
+
+
+module.exports = mongoose.model("Product", productSchema);
