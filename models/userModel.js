1cced5b23fa4392a02fbd8880ac82eec0cf2aaed
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-06-17 01:28:46 +0300

    added refreshToken is user SCHEMA

--- models/userModel.js
+++ models/userModel.js
@@ -40,8 +40,10 @@ const userSchema = new mongoose.Schema({
         default: [],
     },
     address: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
-    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }]
-
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    refreshToken: {
+        type: String,
+    }
 
 
 
