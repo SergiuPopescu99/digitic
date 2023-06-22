ced5a076b28a640bc253af50e28070c72c07ca72
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-06-22 16:20:12 +0300

    added product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createProduct, getProduct } = require('../controller/productCtrl');
+const { createProduct, getProduct, getProducts, updateProduct, deleteProduct } = require('../controller/productCtrl');
 const Router = express.Router();
-
-Router.post('/', createProduct)
-Router.get('/:id', getProduct)
-
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
+Router.post('/', authMiddleware, isAdmin, createProduct)
+Router.get('/', getProducts);
+Router.get('/:id', getProduct);
+Router.put('/:id', authMiddleware, isAdmin, updateProduct);
+Router.delete('/:id', authMiddleware, isAdmin, deleteProduct);
 module.exports = Router;
