b5360aa274ca07baaf85dd3590f7e34150e68fe2
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-06-22 17:55:23 +0300

    new morgan dependency

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const productRouter = require('./routes/productRoute')
 const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser')
+const morgan = require('morgan');
 dbConnect();
-
+app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser())
